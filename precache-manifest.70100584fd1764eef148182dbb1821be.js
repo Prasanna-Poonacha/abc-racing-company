c729c3a733e9a5b255ec4040efec7cc6de42647b
Prasanna Poonacha <ppoonacha@mast.com>
2020-08-04 17:49:19 +0530

    Updates

---
+++ precache-manifest.70100584fd1764eef148182dbb1821be.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19558be7e2547e63cd0d8c95a842bd29",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7ee7919dd087f869039f",
+    "url": "./static/css/main.9f8623f8.chunk.css"
+  },
+  {
+    "revision": "143b065aa7ad9cf3c4d2",
+    "url": "./static/js/2.1d7fccfe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.1d7fccfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ee7919dd087f869039f",
+    "url": "./static/js/main.a1d1a0bc.chunk.js"
+  },
+  {
+    "revision": "1d53150b89bf690dde47",
+    "url": "./static/js/runtime-main.77f7f810.js"
+  },
+  {
+    "revision": "f25ed7f30cc4c8911321e7b7fae269cf",
+    "url": "./static/media/70th_Anniversary_Grand_Prix.f25ed7f3.png"
+  },
+  {
+    "revision": "d25eac98401f77b8313792ee71a7cc7b",
+    "url": "./static/media/Belgian_Grand_Prix.d25eac98.png"
+  },
+  {
+    "revision": "5445a460a1eabcb5c1c0e84062d3fc86",
+    "url": "./static/media/British_Grand_Prix.5445a460.png"
+  },
+  {
+    "revision": "60f320b08358892c8ae43c63120a0467",
+    "url": "./static/media/D1.60f320b0.png"
+  },
+  {
+    "revision": "5808717c8106aee6bc3775681567fa92",
+    "url": "./static/media/D2.5808717c.png"
+  },
+  {
+    "revision": "610578b9c90c8cb81c0d0a8fcf1e2c00",
+    "url": "./static/media/D3.610578b9.png"
+  },
+  {
+    "revision": "e0a375959cc7924f9a1891b2362fbb6b",
+    "url": "./static/media/Spanish_Grand_Prix.e0a37595.png"
+  },
+  {
+    "revision": "d7a3bfc8aa848cbbe426466000494424",
+    "url": "./static/media/bg.d7a3bfc8.jpeg"
+  },
+  {
+    "revision": "9b15d3a9184f711e8b4109b0b50fa38d",
+    "url": "./static/media/car-racing.9b15d3a9.gif"
+  },
+  {
+    "revision": "815862eaff04ec3b7259a5920910b163",
+    "url": "./static/media/car.815862ea.mp4"
+  },
+  {
+    "revision": "594ea7d501be3ed5c6f01dd5bf6f8fda",
+    "url": "./static/media/carx1900x1200-12.594ea7d5.jpeg"
+  },
+  {
+    "revision": "3b619c0c00a533acf6bf52ab8758a349",
+    "url": "./static/media/gal1.3b619c0c.jpeg"
+  },
+  {
+    "revision": "a4e4db9432fefcba757f65894e9a5624",
+    "url": "./static/media/gal3.a4e4db94.jpeg"
+  },
+  {
+    "revision": "de3362010674b841dc9dab1bdbf6c623",
+    "url": "./static/media/icon_fb.de336201.svg"
+  },
+  {
+    "revision": "a13b50a810ee7cfcf8f1b4e883be0e5c",
+    "url": "./static/media/icon_tw.a13b50a8.svg"
+  }
+]);
